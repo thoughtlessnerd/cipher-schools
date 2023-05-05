cf277197ce59dfd8a475c8ef33ac7c4abde0650e
thoughtlessnerd <abhay.csgo001@gmail.com>
2023-05-06 01:12:08 +0530

    api working with no cors error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  let [data, setData] = useState<string | null>(null);
+  useEffect(() => {
+    fetch("http://localhost:8080/products/test")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json.message);
+        console.log(json);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Test</h1>
+      <p>{data ? data : "not fetched"}</p>
+    </>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const product = require("./routes/product.route"); // Imports routes for the products
+const cors = require("cors");
 
 let port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World YAYA");
+});
+
+app.use(cors());
+
+app.use("/products", product);
 
 app.listen(port, () => {
   console.log("We are live on " + port);
